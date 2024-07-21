5aa363bbd01f93c74bd94697a4d2c3f3458fb6ae
adrianquiroga <adrian.quiroga.rodriguez@gmail.com>
2024-07-21 16:06:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,3 @@
 import streamlit as st
-import pandas as pd
-
-df = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-    })
-
-option = st.selectbox(
-    'Which number do you like best?',
-     df['first column'])
-
-'You selected: ', option
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
